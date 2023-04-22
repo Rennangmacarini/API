6341882cf09ac33abd7da27955589fa9edcbf50c
Rennan Macarini <rennangmacarini@gmail.com>
2023-04-22 09:41:56 -0300

    fim do stage8

---
+++ src/routes/tags.routes.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const TagsController = require("../controllers/TagsController")
+
+const TagsRoutes = Router();
+
+const tagsController = new TagsController();
+
+TagsRoutes.get("/:user_id", tagsController.index);
+
+
+
+module.exports = TagsRoutes;
