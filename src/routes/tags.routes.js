8e79fcda4a2ef5f4eb0dea624fbec0ea78f03b94
Rennan Macarini <rennangmacarini@gmail.com>
2023-04-30 18:05:05 -0300

    atualizando api

--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 
 const TagsController = require("../controllers/TagsController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const TagsRoutes = Router();
 
 const tagsController = new TagsController();
 
-TagsRoutes.get("/:user_id", tagsController.index);
+TagsRoutes.get("/",ensureAuthenticated, tagsController.index);
 
 
 
